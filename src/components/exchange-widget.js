62efe5668915b1d3f66010a1451f841a198b5c41
Luca <gianluca.battaglia1@students.mq.edu.au>
2023-04-30 22:37:51 +1000

    CurrencyExchange Update - Linking API & Properties

--- src/components/exchange-widget.js
+++ src/components/exchange-widget.js
@@ -3,6 +3,11 @@ import { LitElement, html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/co
 class CurrencyExchange extends LitElement {
     static properties = {
       header: { type: String },
+      fromCurrency: {type: String},
+      toCurrency: {type: String},
+      amount: {type: Number},
+      result: {type: Number},
+      currencies: {type: Array}
     }
   
     static styles = css`
@@ -18,14 +23,48 @@ class CurrencyExchange extends LitElement {
     constructor() {
       super();
       this.header = 'Currency Exchange';
+      this.amount = 0;
+      this.result = 0;
+      this.fromCurrency = 'USD';
+      this.toCurrency = 'AUD';
+      this.currencies = [];
+    }
+
+
+    
+    requestCurrencyArray(){
+      const ARRAY_URL = 'https://api.exchangerate.host/latest';
+      const request = new XMLHttpRequest();
+      request.open('GET', this.ARRAY_URL);
+      request.responseType = 'json';
+      request.send();
+      request.onload = () => {
+        const response = request.response;
+        this.currencies = Object.keys(response.rates);
+        this.resultUpdate();
+      }
+    }
+
+    currencyConverter(){
+      
+
     }
   
     render() {
       return html`
-        <div>
-            <h3>${this.header}</h3>
-            <slot></slot>
-        </div>      
+        <label>From:</label>
+        <select @change="${(e) => this.fromCurrency = e.target.value}> 
+          ${this.currencies.map(currency => html` <option value="${currency}"> ${currency} </option>`)}
+        </select>
+
+       
+
+        <br>
+        <input id="from-input" type="number" .value="${this.amount}" @input="${(e) => this.amount = e.target.value}">
+        <button @click="${this.currencyConverter}"> Convert </button>
+        <br>
+
+
       `;
     }
   }

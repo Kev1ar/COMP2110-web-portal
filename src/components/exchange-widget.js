d5f5f8bc26b4823a98a9d94a461bea21bd5ff046
Luca <gianluca.battaglia1@students.mq.edu.au>
2023-05-01 15:58:01 +1000

    ExchangeWidget -Working Currency Converter

--- src/components/exchange-widget.js
+++ src/components/exchange-widget.js
@@ -23,48 +23,69 @@ class CurrencyExchange extends LitElement {
     constructor() {
       super();
       this.header = 'Currency Exchange';
-      this.amount = 0;
-      this.result = 0;
-      this.fromCurrency = 'USD';
-      this.toCurrency = 'AUD';
+      this.amount = 0.01;
+      this.result = 0.01;
+      this.fromCurrency = 'AED';
+      this.toCurrency = 'AED';
       this.currencies = [];
+      this.requestCurrencyArray();
     }
 
 
     
     requestCurrencyArray(){
-      const ARRAY_URL = 'https://api.exchangerate.host/latest';
+      const requestURL  = 'https://api.exchangerate.host/latest';
       const request = new XMLHttpRequest();
-      request.open('GET', this.ARRAY_URL);
+      request.open('GET', requestURL);
       request.responseType = 'json';
       request.send();
       request.onload = () => {
         const response = request.response;
         this.currencies = Object.keys(response.rates);
-        this.resultUpdate();
+        this.requestUpdate();
       }
     }
 
-    currencyConverter(){
-      
+    connectedCallBack(){
+      super.connectedCallBack()
+      requestCurrencyArray();
+      currencyConverter();
+    }
 
+     
+      
+    currencyConverter(){
+      const requestURL = `https://api.exchangerate.host/convert?from=${this.fromCurrency}&to=${this.toCurrency}&amount=${this.amount}`;
+      const request = new XMLHttpRequest();
+      request.open('GET', requestURL);
+      request.responseType = 'json';
+      request.send();
+      request.onload = () => {
+        this.result = request.response.result;
+        this.requestUpdate();
+      }
     }
   
     render() {
       return html`
-        <label>From:</label>
-        <select @change="${(e) => this.fromCurrency = e.target.value}> 
-          ${this.currencies.map(currency => html` <option value="${currency}"> ${currency} </option>`)}
+        <h3>${this.header}</h3>
+        <label for="from-currency">From:</label>
+        <select id="from-currency" @change="${(e) => this.fromCurrency = e.target.value}"> 
+          ${this.currencies.map(currency => html`<option value="${currency}"> ${currency} </option>`)}
         </select>
 
-       
 
+        <label for="to-currency">To:</label>
+        <select id="to-currency" @change="${(e) => this.toCurrency = e.target.value}"> 
+          ${this.currencies.map(currency => html`<option value="${currency}"> ${currency} </option>`)}
+        </select>
+        <input id="from-input" type="Number" .value="${this.amount}" @input="${(e) => this.amount = e.target.value}">
         <br>
-        <input id="from-input" type="number" .value="${this.amount}" @input="${(e) => this.amount = e.target.value}">
+
+        <label for="to-output">Result:</label>
         <button @click="${this.currencyConverter}"> Convert </button>
         <br>
-
-
+        <div class="result">${this.result.toFixed(2)}</div>
       `;
     }
   }
